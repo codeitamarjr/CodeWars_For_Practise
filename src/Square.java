714cec646d2d049eedfb48bed66d8ccda588caa8
Itamar Junior <itjunior@gmail.com>
2021-05-04 23:10:41 +0100

    Initial commit

---
+++ src/Square.java
@@ -0,0 +1,17 @@
+public class Square {
+    public static boolean isSquare(int n) {
+        int count;
+        boolean squareTestResult;
+
+        if(n>0) {
+
+            for (count=1; (count*count)==n | count <= n; count++) {
+                boolean squareResult = false;
+            }
+
+
+
+        } else
+            return false;
+    }
+}
